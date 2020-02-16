7cf5bc05b522e390b12714755e838dc39907b2df
Steve Worcester <Worcester.Steve@Gmail.com>
2020-02-16 14:43:27 -0800

    DummyScript-Organization

--- Assets/Scripts/Terrain/UnpathableTerrain.cs
+++ Assets/Scripts/Terrain/UnpathableTerrain.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class UnpathableTerrain : MonoBehaviour
 {
+    // pathable = false
+    // LOS block bool?
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Units/Move-MeleeUnit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Turn counter per square moved or per move action (bool? + counter)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the map size
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // A* algorithm When movement is selected to get movement area. 
+        // Display movement area
+        // A* algorithm to show path when movement square is selected
+        // Accept? w/e
+    }
+}
