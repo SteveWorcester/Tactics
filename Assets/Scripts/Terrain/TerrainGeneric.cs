efe84549c31714fa381ab7c9ac7397d35068e5bc
Steve Worcester <Worcester.Steve@Gmail.com>
2020-04-15 19:33:29 -0700

    Add Generic Terrain script.
    - Script contains basic terrain info, including Move Difficulty, Tile Height, Pathable/Unpathable, and LOS Blocker.
    - Generic terrain script added as a component to all current terrain tiles.

--- Assets/Scripts/Terrain/TerrainGeneric.cs
+++ Assets/Scripts/Terrain/TerrainGeneric.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathableTerrain1 : MonoBehaviour
+public class TerrainGeneric : MonoBehaviour
 {
     // regular movement? difficult movement? add to turn counter? reduce move by X instead of block?
     // LOS block bool?
+    public float MoveDifficulty;
+    public float TileHeight;
+    public bool Pathable;
+    public bool BlocksLineOfSight;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(MoveDifficulty);
     }
 
     // Update is called once per frame
