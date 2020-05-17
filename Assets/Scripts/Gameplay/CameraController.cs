b2d38b9e7bba5ee18ee85cabea9c48a154c47ebd
Steve Worcester <Worcester.Steve@Gmail.com>
2020-05-17 14:47:50 -0700

    Camera Zoom
    - Camera zoom implemented with customizable zoom values.
    - Please attach to camera and modify with values you see as appropriate.
    
    Functionality:
    - Start of game: Camera starts in a mid position, and LERP zooms out to the "Zoomed Out" position
    - Zoom In/Out: "Mouse ScrollWheel" joystick axis (Project Settings -> Input) zooms the camera. ScrollUp = Zoom In; ScrollDown = Zoom Out.
    -- Only two states are available in-game: "Zoomed In" and "Zoomed Out"; there is not dynamic zoom. Camera LERP moves between the two zoom locations.

---
+++ Assets/Scripts/Gameplay/CameraController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
+
+public class CameraController : MonoBehaviour
+{   
+    // modify these
+    public float cameraTransitionTimeInSeconds = 1f;
+    public float zoomForwardBackwardChange = 2f;
+    public float zoomUpDownChange = 2f;
+    
+    // ======do not modify these=====
+    private bool zoomedOut = false;
+    private Vector3 newCameraLocation;
+    // ==============================
+    private void Start()
+    {
+        ZoomOut();
+    }
+    void Update()
+    {
+    
+        if ((zoomedOut == true) && (Input.GetAxis("Mouse ScrollWheel") > 0))
+        {
+            ZoomIn();
+        }
+        if ((zoomedOut == false) && (Input.GetAxis("Mouse ScrollWheel") < 0) )
+        {
+            ZoomOut();
+        }
+
+        }
+    public void ZoomOut()
+    {        
+        Debug.Log("Zooming camera out");
+        var currentPos = transform.position;
+        var newCameraForwardLocation = -Vector3.forward * zoomForwardBackwardChange;
+        var newCameraDownLocation = -Vector3.down * zoomUpDownChange;
+        newCameraLocation = newCameraForwardLocation + newCameraDownLocation;
+        
+
+        StartCoroutine(LerpFromTo(currentPos, newCameraLocation, cameraTransitionTimeInSeconds));
+        zoomedOut = true;
+    }
+    
+    public void ZoomIn()
+    {        
+        Debug.Log("Zooming camera in");
+        var currentPos = transform.position;
+        var newCameraForwardLocation = Vector3.forward * zoomForwardBackwardChange;
+        var newCameraDownLocation = Vector3.down * zoomUpDownChange;
+        newCameraLocation = newCameraForwardLocation + newCameraDownLocation;        
+
+        StartCoroutine(LerpFromTo(currentPos, newCameraLocation, cameraTransitionTimeInSeconds));
+        zoomedOut = false;
+    }
+
+    IEnumerator LerpFromTo(Vector3 pos1, Vector3 pos2, float duration)
+    {
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            transform.position = Vector3.Lerp(pos1, pos2, t / duration);
+            yield return 0;
+        }
+        transform.position = pos2;
+    }
+}
