e27abc05a30fc40dc6c1bf0c8de88acb49220869
Steve Worcester <Worcester.Steve@Gmail.com>
2020-05-17 19:06:55 -0700

    Camera Rotate
    - Rotation is hooked up to < and > keys (no need to press shift. they are actually , and .)
    - Rotation currently has a known issue: the first rotation is 45 degrees, but every rotation from then on is either just over or just under 45 degrees.

--- Assets/Scripts/Gameplay/CameraController.cs
+++ Assets/Scripts/Gameplay/CameraController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Net.Http.Headers;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.UIElements;
@@ -6,13 +7,20 @@ using UnityEngine.UIElements;
 public class CameraController : MonoBehaviour
 {   
     // modify these
-    public float cameraTransitionTimeInSeconds = .25f;
+    public float zoomTransitionTimeInSeconds = .25f;
     public float zoomForwardBackwardChange = 2f;
     public float zoomUpDownChange = 2f;
+
+    public float rotateTransitionTimeInSeconds = .25f;
+    public float rotateDegrees = 45f; 
     
     // ======do not modify these=====
     private bool zoomedOut = false;
-    private Vector3 newCameraLocation;
+    private Vector3 newZoomCameraLocation;
+
+    private Quaternion currentCameraRotationLocation;
+    private Quaternion newCameraRotationLocation;
+    private bool currentlyRotating = false;
     // ==============================
     private void Start()
     {
@@ -29,18 +37,66 @@ public class CameraController : MonoBehaviour
         {
             ZoomOut();
         }
-
+        if (Input.GetKeyDown(KeyCode.Comma) && currentlyRotating == false)
+        {
+            currentlyRotating = true;
+            RotateLeft();
         }
+        if (Input.GetKeyDown(KeyCode.Period) && currentlyRotating == false)
+        {
+            currentlyRotating = true;
+            RotateRight();
+        }
+    }
+
+    /// <summary>
+    /// Move camera in a straight line
+    /// </summary>
+    /// <param name="startingPosition"></param>
+    /// <param name="endingPosition"></param>
+    /// <param name="durationOfMove"></param>
+    /// <returns></returns>
+    IEnumerator LerpFromTo(Vector3 startingPosition, Vector3 endingPosition, float durationOfMove)
+    {
+        Debug.Log("Lerping camera");
+        for (float t = 0f; t < durationOfMove; t += Time.deltaTime)
+        {
+            transform.position = Vector3.Lerp(startingPosition, endingPosition, t / durationOfMove);
+            yield return 0;
+        }
+        transform.position = endingPosition;
+    }
+
+    /// <summary>
+    /// Rotate camera around a sphere
+    /// </summary>
+    /// <param name="startingPosition"></param>
+    /// <param name="endingPosition"></param>
+    /// <param name="durationOfMove"></param>
+    /// <returns></returns>
+    IEnumerator SlerpFromTo(Quaternion startingPosition, Quaternion endingPosition, float durationOfMove)
+    {
+        Debug.Log("Slerping camera");
+        for (float t = 0f; t < durationOfMove; t += Time.deltaTime)
+        {
+            transform.rotation = Quaternion.Slerp(startingPosition, endingPosition, t / durationOfMove);
+            yield return 0;
+        }
+        transform.rotation = endingPosition;
+    }
+
+    #region Zoom
+
     public void ZoomOut()
     {        
         Debug.Log("Zooming camera out");
         var currentPos = transform.position;
         var newCameraForwardLocation = -Vector3.forward * zoomForwardBackwardChange;
         var newCameraDownLocation = -Vector3.down * zoomUpDownChange;
-        newCameraLocation = newCameraForwardLocation + newCameraDownLocation;
+        newZoomCameraLocation = newCameraForwardLocation + newCameraDownLocation;
         
 
-        StartCoroutine(LerpFromTo(currentPos, newCameraLocation, cameraTransitionTimeInSeconds));
+        StartCoroutine(LerpFromTo(currentPos, newZoomCameraLocation, zoomTransitionTimeInSeconds));
         zoomedOut = true;
     }
     
@@ -50,19 +106,35 @@ public class CameraController : MonoBehaviour
         var currentPos = transform.position;
         var newCameraForwardLocation = Vector3.forward * zoomForwardBackwardChange;
         var newCameraDownLocation = Vector3.down * zoomUpDownChange;
-        newCameraLocation = newCameraForwardLocation + newCameraDownLocation;        
+        newZoomCameraLocation = newCameraForwardLocation + newCameraDownLocation;        
 
-        StartCoroutine(LerpFromTo(currentPos, newCameraLocation, cameraTransitionTimeInSeconds));
+        StartCoroutine(LerpFromTo(currentPos, newZoomCameraLocation, zoomTransitionTimeInSeconds));
         zoomedOut = false;
     }
 
-    IEnumerator LerpFromTo(Vector3 pos1, Vector3 pos2, float duration)
-    {
-        for (float t = 0f; t < duration; t += Time.deltaTime)
-        {
-            transform.position = Vector3.Lerp(pos1, pos2, t / duration);
-            yield return 0;
-        }
-        transform.position = pos2;
+    #endregion
+
+    #region Rotate
+
+    public void RotateLeft()
+    {        
+        Debug.Log("Rotating camera left");
+        currentCameraRotationLocation = transform.rotation;
+        newCameraRotationLocation = currentCameraRotationLocation * Quaternion.Euler(transform.rotation.x, transform.rotation.y + rotateDegrees, transform.rotation.z);
+
+        StartCoroutine(SlerpFromTo(currentCameraRotationLocation, newCameraRotationLocation, rotateTransitionTimeInSeconds));
+        currentlyRotating = false;
     }
+
+    public void RotateRight()
+    {
+        Debug.Log("Rotating camera right");
+        currentCameraRotationLocation = transform.rotation;
+        newCameraRotationLocation = currentCameraRotationLocation * Quaternion.Euler(transform.rotation.x, transform.rotation.y - rotateDegrees, transform.rotation.z);
+
+        StartCoroutine(SlerpFromTo(currentCameraRotationLocation, newCameraRotationLocation, rotateTransitionTimeInSeconds));
+        currentlyRotating = false;
+    }
+
+    #endregion
 }
